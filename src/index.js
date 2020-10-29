a18cb66145cd5a0cb30c6ee91a39a43362525431
Yağızhan <yagizhan49@protonmail.com>
2020-10-29 14:06:48 +0300

    Node version check on the main file, will explain why in the PR

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+// This will check if the node version you are running is the required
+// Node version or higher, if it isn't it will throw the following error to inform you.
+if (Number(process.version.slice(1).split(".")[0]) < 12) throw new Error("Node 12.0.0 or higher is required. You need to update your Node.js to the required release, or a newer one.");
+
 // First, we require discord.js's Client and Collection constructors
 const { Client, Collection } = require("discord.js")
 // Now, we initialize a new discord.js Client
